5a484e16e6ff6f67d30304b7101cd1d3a66cdca2
dd-tech-nz <ddumolo@hotmail.com>
2020-05-23 14:17:08 +1200

    create collection-item.component for shop page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './collection-item.styles.scss'
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
-  <div className='collection-item' style={{ display: 'inlineBlock' }}>
+  <div className='collection-item'>
     <div
       className='image'
       style={{
