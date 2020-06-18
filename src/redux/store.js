0b542ef87f3b63eb73f18bfb31317eb3fc34fb61
dd-tech-nz <ddumolo@hotmail.com>
2020-06-18 12:54:07 +1200

    implement root-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,8 @@ import { persistStore } from 'redux-persist'
 // import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga'
 
-import { fetchCollectionsStart } from './shop/shop.sagas'
-
 import rootReducer from './root-reducer'
+import rootSaga from './root-saga'
 
 const sagaMiddleware = createSagaMiddleware()
 
@@ -19,7 +18,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-sagaMiddleware.run(fetchCollectionsStart)
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store)
 
