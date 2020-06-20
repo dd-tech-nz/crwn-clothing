2b2a35292cd28656c4bd51f049aa47e1a3ee0f9d
dd-tech-nz <ddumolo@hotmail.com>
2020-06-20 20:34:27 +1200

    create email sign in saga

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import CheckoutPage from './pages/checkout/checkout.component'
-import {
-  auth,
-  createUserProfileDocument
-} from './firebase/firebase.utils'
-import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors'
 
 
@@ -21,7 +16,6 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props
     
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     //   if (userAuth) {
@@ -74,10 +68,5 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 })
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
